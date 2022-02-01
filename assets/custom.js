e688c0b46ddeda4e28e3245031eb861a1b12c15b
MunirYounos <eaaamy@gmail.com>
2022-02-01 14:33:22 +0100

    initial commit

---
+++ assets/custom.js
@@ -0,0 +1,149 @@
+(function(a){
+	$('.youtube-cover-image, .youtube-button').on('click', function (e) {
+		$('.youtube-cover-image , .youtube-button').hide();
+	});
+	// scroll smothing for readmore button
+	$('.btn-readmore').on('click', function () {
+		$('html, body').animate({ scrollTop: $(this.hash).offset().top - 80 }, 1000);
+		return false;
+	});
+
+})(jQuery);
+
+
+// variables
+var accordionBtn = document.querySelectorAll('.accordion__title');
+var allTexts = document.querySelectorAll('.accordion__flex');
+var accIcon = document.querySelectorAll('.accIcon');
+
+// event listener
+accordionBtn.forEach(function (el) {
+	el.addEventListener('click', toggleAccordion)
+});
+
+// function
+function toggleAccordion(el) {
+	var targetText = el.currentTarget.nextElementSibling.classList;
+	var targetAccIcon = el.currentTarget.children[0];
+	var target = el.currentTarget;
+
+	if (targetText.contains('accordion-show')) {
+		targetText.remove('accordion-show');
+		targetAccIcon.classList.remove('anime');
+		target.classList.remove('accordionTitleActive');
+	}
+	else {
+		accordionBtn.forEach(function (el) {
+			el.classList.remove('accordionTitleActive');
+
+			allTexts.forEach(function (el) {
+				el.classList.remove('accordion-show');
+			})
+
+			accIcon.forEach(function (el) {
+				el.classList.remove('anime');
+			})
+
+		})
+
+		targetText.add('accordion-show');
+		target.classList.add('accordionTitleActive');
+		targetAccIcon.classList.add('anime');
+	}
+}
+
+//tabs section
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.content-body');
+function selectItem(e) {
+  removeBorder();
+  removeShow();
+  this.classList.add('tab-active');
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  tabContentItem.classList.add('show-active');
+}
+
+function removeBorder() {
+  tabItems.forEach(item => {
+    item.classList.remove('tab-active');
+  });
+}
+// Remove show class from all content items
+function removeShow() {
+  tabContentItems.forEach(item => {
+    item.classList.remove('show-active');
+  });
+}
+// Listen for tab item click
+tabItems.forEach(item => {
+  item.addEventListener('click', selectItem);
+});
+
+// relief tabs
+const homeTabBtns = document.querySelectorAll('.home-tabs__wrapper-btns-btn');
+const homeTabItems = document.querySelectorAll('.home-tabs__wrapper-content-img');
+
+function selectHomeTabContents(e) {
+  removeActiveHomeTabClass();
+  removeActiveHomeTabItemClass();
+  this.classList.add('active');
+  const homeTabContentItems = document.querySelector(`#${this.id}-content`);
+  homeTabContentItems.classList.add('active');
+}
+
+homeTabBtns.forEach(tabbtn => tabbtn.addEventListener('click', selectHomeTabContents));
+
+const removeActiveHomeTabClass = () => {
+  homeTabBtns.forEach(tabbtn => {
+    tabbtn.classList.remove('active')
+  })
+}
+const removeActiveHomeTabItemClass = () => {
+  homeTabItems.forEach(homeTabItem => {
+    homeTabItem.classList.remove('active')
+  })
+}
+//topbar counter
+let announcementCounterEndText = document.querySelector('.announcement-end-text');
+let announcementEndTextBack = document.querySelector('.announcement-message');
+const announcementDateId = document.getElementById('announcement_counter');
+let announcementEventDate = announcementDateId.getAttribute('data-announcement-date');
+function TopBarUpdateTimer(at, someId ) {
+future = Date.parse(at);
+now = new Date();
+diff = future - now;
+
+if(diff >= 0){
+days = Math.floor(diff / (1000 * 60 * 60 * 24));
+hours = Math.floor(diff / (1000 * 60 * 60));
+mins = Math.floor(diff / (1000 * 60));
+secs = Math.floor(diff / 1000);
+	//add zeros
+	function addZero(num) {
+		return ("0" + parseInt(num)).substr(-2);
+	}
+d = addZero(days);
+h = addZero(hours - days * 24);
+m = addZero(mins - hours * 60);
+s = addZero(secs - mins * 60);
+  if(d<1){
+     someId.innerHTML = `
+    <div class="announcement_number"><span class="digit">${h}</span><span class="announcement_text">H<span class="hide-small">ours</span></span></div>
+    <div class="announcement_number"><span class="digit">${m}</span><span class="announcement_text">M<span class="hide-small">inuttes</span></span></div>
+    <div class="announcement_number"><span class="digit">${s}</span><span class="announcement_text">S<span class="hide-small">econds</span></span></div>
+    `
+  }else{
+    someId.innerHTML = `
+    <div class="announcement_number"><span class="digit">${d}</span><span class="announcement_text">D<span class="hide-small">ays</span></span></div>
+    <div class="announcement_number"><span class="digit">${h}</span><span class="announcement_text">H<span class="hide-small">ours</span></span></div>
+    <div class="announcement_number"><span class="digit">${m}</span><span class="announcement_text">M<span class="hide-small">inuttes</span></span></div>
+    <div class="announcement_number"><span class="digit">${s}</span><span class="announcement_text">S<span class="hide-small">econds</span></span></div>
+    `
+  }
+	
+}else {
+	announcementEndTextBack.innerHTML = `${announcementCounterEndText.innerHTML}`;
+	someId.innerHTML ='';
+}
+}
+setInterval('TopBarUpdateTimer(announcementEventDate, announcementDateId  )', 1000);
---
+++ assets/uk-counter.js
@@ -0,0 +1,117 @@
+//be counter
+let domTopIdnl = document.getElementById('uk___js-counter');
+if (domTopIdnl !== null) {
+	weekDayNames(domTopIdnl);
+}
+
+function weekDayNames(Id1) {
+	let d = new Date();
+	weekday = d.getDay(),
+		dayslabel = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+		weekday = dayslabel[d.getDay()];
+	//switcher
+	switch (weekday) {
+		case 'Fri':
+			calculateDeliveryTimeFriday(Id1, d, 14);
+			break;
+		case 'Sat':
+			calculateDeliveryTimeSaturday(Id1)
+			break;
+		case 'Sun':
+			calculateDeliveryTimeEveryDay(Id1, d, 0);
+			break;
+		default:
+			calculateDeliveryTimeEveryDay(Id1, d, 14);
+	}
+}
+function calculateDeliveryTimeEveryDay(someId, currentDate, setTime) {
+	let putDateOfNow = currentDate,
+		startDateOfToday = putDateOfNow.setHours(setTime, 0, 0),
+		endDateOfToday = putDateOfNow.setHours(23, 59, 59);
+	//add zeros
+	function addZero(num) {
+		return ("0" + parseInt(num)).substr(-2);
+	}
+	function tick() {
+		let now = new Date();
+		if (now <= startDateOfToday) {
+			let remain = ((startDateOfToday - now) / 1000),
+				hours = addZero((remain / 60 / 60) % 60),
+				min = addZero((remain / 60) % 60),
+				sec = addZero(remain % 60);
+   
+        someId.innerHTML = `<div class="timer__wrapper">
+        <div class="timer__text">
+				Get your order shipped today - order within:
+        </div>
+        <div class="timer__counter">  
+        <span class="time-go">
+        <span class="time-hour"> <strong>${hours} </strong><span class="time__badge">Hours</span></span> <code>:</code> 
+        <span class="time-min"> <strong>${min}</strong><span class="time__badge">Minutes</span></span> <code>:</code> 
+        <span class="time-sec"> <strong>${sec}</strong><span class="time__badge">Seconds</span></span> 
+        </span></div></div>`;
+     
+
+			setTimeout(tick, 1000);
+		} else if (now < endDateOfToday) {
+			if (now >= endDateOfToday) { // too late, go to tomorrow
+				after.setDate(after.getDate() + 1);
+			}
+			let remain = ((endDateOfToday - now) / 1000),
+				hours = addZero((remain / 60 / 60) % 60),
+				min = addZero((remain / 60) % 60),
+				sec = addZero(remain % 60);
+        someId.innerHTML = `<div class="timer__wrapper">
+        <div class="timer__text">
+      We are sending your parcel tomorrow - order within:
+        </div>
+        <div class="timer__counter">
+        <span class="time-up"  >
+        <span class="time-hour"> <strong>${hours} </strong><span class="time__badge">Hours</span></span> <code>:</code>  
+        <span class="time-min"> <strong>${min}</strong><span class="time__badge">Minutes</span></span> <code>:</code> 
+        <span class="time-sec"> <strong>${sec}</strong><span class="time__badge">Seconds</span></span> 
+        </span></div></div>`;
+  
+			setTimeout(tick, 1000);
+		}
+	}
+	tick();
+}
+function calculateDeliveryTimeFriday(someId, currentDate, setTime) {
+	let putDateOfNow = currentDate,
+		startDateOfToday = putDateOfNow.setHours(setTime, 0, 0);
+	//add zeros
+	function addZero(num) {
+		return ("0" + parseInt(num)).substr(-2);
+	}
+	function tick() {
+		let now = new Date();
+		if (now <= startDateOfToday) {
+			let remain = ((startDateOfToday - now) / 1000),
+				hours = addZero((remain / 60 / 60) % 60),
+				min = addZero((remain / 60) % 60),
+				sec = addZero(remain % 60);
+ 
+        someId.innerHTML = `<div class="timer__wrapper">
+        <div class="timer__text">
+        Get your order shipped today - order within:
+        </div>
+        <div class="timer__counter">  
+        <span class="time-go">
+        <span class="time-hour"> <strong>${hours} </strong><span class="time__badge">Hours</span></span> <code>:</code> 
+        <span class="time-min"> <strong>${min}</strong><span class="time__badge">Minutes</span></span> <code>:</code> 
+        <span class="time-sec"> <strong>${sec}</strong><span class="time__badge">Seconds</span></span> 
+        </span></div></div>`;
+  
+			setTimeout(tick, 1000);
+		} else if (now > startDateOfToday) {
+
+			someId.innerHTML = `<div class="timer__wrapper"><div class="timer__text">Free delivery on orders over 59,90 &pound; I Free return and exchange</div></div>`;
+    
+		}
+	}
+	tick();
+}
+function calculateDeliveryTimeSaturday(someId) {
+    someId.innerHTML = `<div class="timer__wrapper"><div class="timer__text">Free delivery on orders over 59,90 &pound; I Free return and exchange</div></div>`;
+}
